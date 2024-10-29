be29a6e47d20f0f85a8eb0928daac5aa005f3e8c
VineelaShaik <vineelalatheef@gmail.com>
2024-10-29 11:58:21 +0530

    nodejs

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,11 +1,13 @@
 const router=require('express').Router()
-app.get("/readAllBlogPosts",(req,res)=>
+router.get("/readAllBlogPosts",(req,res)=>
         {  let qry= req.query
             res.send(`QuereyString : ${qry.id}, ${qry.name}`)
             
         });
-app.post("/readAllBlogPosts",(req,res)=>
+        router.post("/readAllBlogPosts",(req,res)=>
         {  let qry= req.body
             res.send(`URL ENCODED : ${qry.TITLE}, ${qry.DESC}`)
             
         });
+
+        module.exports = router;
